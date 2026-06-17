18f77406ed550201795e0180976c937061123b60
pmqs98 <pedromqsousa@hotmail.com>
2026-06-17 23:06:47 +0100

    config files added

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+// eslint.config.mjs
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error",
+    },
+  }
+);
